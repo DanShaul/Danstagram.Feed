47f1c617076ce1d2feff102bf7eb5f0ee482d872
DanShaul <dansh7836@gmail.com>
2023-11-28 19:48:25 +0200

    Finished

--- src/Danstagram.Feed.Service/Consumers/LikeCreatedConsumer.cs
+++ src/Danstagram.Feed.Service/Consumers/LikeCreatedConsumer.cs
@@ -6,36 +6,39 @@ using MassTransit;
 
 namespace Danstagram.Feed.Service.Consumers
 {
-  public class LikeCreatedConsumer : IConsumer<LikeCreated>
-  {
-    #region Constructors
-    public LikeCreatedConsumer(IRepository<LikeItem> repository){
-      this.repository = repository;
-    }
-
-
-    #endregion
-
-    #region Properties
-    private IRepository<LikeItem> repository;
-    #endregion
-
-    #region Methods
-    public async Task Consume(ConsumeContext<LikeCreated> context)
+    public class LikeCreatedConsumer : IConsumer<LikeCreated>
     {
-      var message = context.Message;
-      if (await repository.GetAsync(message.Id) != null)
-        return;
+        #region Constructors
+        public LikeCreatedConsumer(IRepository<LikeItem> repository)
+        {
+            this.repository = repository;
+        }
 
-      LikeItem likeItem = new()
-      {
-        Id = message.Id,
-        UserId = message.UserId,
-        FeedItemId = message.FeedItemId
-      };
 
-      await repository.CreateAsync(likeItem);
+        #endregion
+
+        #region Properties
+        private readonly IRepository<LikeItem> repository;
+        #endregion
+
+        #region Methods
+        public async Task Consume(ConsumeContext<LikeCreated> context)
+        {
+            LikeCreated message = context.Message;
+            if (await repository.GetAsync(message.Id) != null)
+            {
+                return;
+            }
+
+            LikeItem likeItem = new()
+            {
+                Id = message.Id,
+                UserId = message.UserId,
+                FeedItemId = message.FeedItemId
+            };
+
+            await repository.CreateAsync(likeItem);
+        }
+        #endregion
     }
-    #endregion
-  }
 }
